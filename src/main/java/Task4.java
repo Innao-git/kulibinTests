eb018822aa0b6ee996c41f3ffc9a4c1d73207768
Innao-git <Innatsapana1@gmail.com>
2022-04-13 01:39:14 +0300

    small improvements

---
+++ src/main/java/Task4.java
@@ -0,0 +1,78 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+/*4. В разделе "Электроинструменты" / "Болгарки"
+Для 10 рандомных товаров с акционным тикетом (может быть % скидки или Акция) провести расчет
+акционной цены относительно старой используя процент скидки.
+В assert упавшего теста вывести наименование товара его ожидаемую и фактическую цену.*/
+public class Task4 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver_100_0_4896_60\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        try {
+            driver.get("https://kulibin.com.ua");
+            Thread.sleep(2000);
+            WebElement aElektroinstrument = driver.findElement(By.xpath("//a[@href='/catalog/elektroinstrument/']"));
+
+            Actions action = new Actions(driver);
+            //Performing the mouse hover action on the target element.
+            action.moveToElement(aElektroinstrument).clickAndHold().build().perform();
+            //click on bolgarki
+            WebElement bolgarkiMenu = driver.findElement(By.xpath("//a[@href='/catalog/bolgarki/']"));
+            action.moveToElement(bolgarkiMenu).click().build().perform();
+            List<WebElement> bolgarkiWholeList;
+
+            do {
+                bolgarkiWholeList = driver.findElements(By.xpath("//ul/li[@class='col-xs-4 js-product']"));
+                //if item get class image_sticker_discount
+                for (WebElement item : bolgarkiWholeList) {
+                    try {
+                        WebElement discountItem = item.findElement(By.xpath("./span[contains(@class, 'image_sticker_discount')]"));
+
+                        String discount = discountItem.getText();
+                        String name = item.findElement(By.className("s_title")).getText();
+                        /*WebElement priceRow = (new WebDriverWait(driver, Duration.ofSeconds(5))
+                                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class='price-row']"))));
+                        String oldPrice = priceRow.findElement(By.xpath(".//span[@class='item_old_price old-price']")).getText();
+                        String price = priceRow.findElement(By.xpath(".//span[@class='price']")).getText();*/
+                        System.out.println(name + " " + discount);
+                    } catch (Exception e) {
+                        continue;
+                    }
+                }
+
+                WebElement nextPageButton = driver.findElement(By.cssSelector("div.paging a.next.btn-blue"));
+                new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(nextPageButton));
+                if (nextPageButton.isEnabled()) {
+                    nextPageButton.click();
+                    new WebDriverWait(driver, Duration.ofSeconds(30)).until(
+                            webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+                } else {
+                    break;
+                }
+
+            } while ((bolgarkiWholeList.size() != 0));
+            //add check if button is desabled
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            //Thread.sleep(20000);
+            driver.quit();
+        }
+    }
+}
