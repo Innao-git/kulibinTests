459cbf8f2aa57a6802d2fadfe9eb9da659639634
Innao-git <Innatsapana1@gmail.com>
2022-04-12 12:47:28 +0300

    start

--- src/main/java/Task3.java
+++ src/main/java/Task3.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Task3 {
+/*
+3. Перейти в раздел "Электроинструменты" / "Шуруповерты"
+        Вывести "Наименование" всех товаров у которых есть иконка с американским флагом на первых 3х страницах
+*/
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Task3 {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver_100_0_4896_60\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        try {
+            driver.get("https://kulibin.com.ua");
+            Thread.sleep(2000);
+            WebElement aElektroinstrument = driver.findElement(By.xpath("//a[@href='/catalog/elektroinstrument/']"));
+
+            Actions action = new Actions(driver);
+            //Performing the mouse hover action on the target element.
+            action.moveToElement(aElektroinstrument).clickAndHold().build().perform();
+            //click on shurupoverty
+            WebElement shurupoverty = driver.findElement(By.xpath("//a[@href='/catalog/shurupoverty/']"));
+            action.moveToElement(shurupoverty).click().build().perform();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+
+    }
 }
